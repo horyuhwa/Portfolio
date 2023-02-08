b0d70afecf1bc8f46d4c3d321e21d935171c4004
horyuhwa <hrodrvitnir@gmail.com>
2023-02-08 12:51:13 +0900

    update

--- OHALL_sns_team_project (eclips, mysql, spring, d3chart)/SNS_project_OHALL/web-project-main/sns/src/main/java/com/project/sns/d3/d3s.java
+++
@@ -1,9 +0,0 @@
-package com.project.sns.d3;
-
-import java.util.List;
-import java.util.Map;
-
-public interface d3s {
-	List<Map<String, Object>> segu_select(Map<String, Object> map);
-	List<Map<String, Object>> susunggu_select(Map<String, Object> map);
-}
